1213177f3066c2c35acc867aa54eedb1dbd4d109
WJKw0k <snappytheturtle101@gmail.com>
2023-05-14 01:15:22 -0700

    Update API with cors allow policies

--- goBackend/main.go
+++ goBackend/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,6 +18,19 @@ var curriculumNames = []string{"Astronautics", "Aeronautics", "GRC for Packaging
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://localhost"},
+		AllowMethods:     []string{"POST, OPTIONS, GET, PUT"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
 	router.GET("/curriculum", getCurriculum)
 	router.POST("/curriculum", validateClasses)
 
@@ -24,21 +39,12 @@ func main() {
 
 // getCurriculum responds with a list of the available curriculums
 func getCurriculum(c *gin.Context) {
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 	c.IndentedJSON(http.StatusOK, curriculumNames)
 }
 
 // validateClasses checks to see if the classContext fulfills the requirement for a degree
 func validateClasses(c *gin.Context) {
 	var valRequest classContext
-
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
 	// Call BindJSON to bind the received JSON to
 	// newAlbum.
