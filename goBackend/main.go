e5e0056d8d1cf7480379343055bc70765546face
WJKw0k <snappytheturtle101@gmail.com>
2023-05-14 01:15:22 -0700

    Update API with cors allow policies

--- goBackend/main.go
+++ goBackend/main.go
@@ -24,12 +24,21 @@ func main() {
 
 // getCurriculum responds with a list of the available curriculums
 func getCurriculum(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 	c.IndentedJSON(http.StatusOK, curriculumNames)
 }
 
 // validateClasses checks to see if the classContext fulfills the requirement for a degree
 func validateClasses(c *gin.Context) {
 	var valRequest classContext
+
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
 	// Call BindJSON to bind the received JSON to
 	// newAlbum.
